0cdf65e4a5fccd92861f676ea78cfb0b98c8b860
Vitor Zen <vitorzen19@gmail.com>
2026-03-09 17:25:34 -0300

    Initial commit

---
+++ src/main/java/com/zendev/moodflix/MoodFlixApplication.java
@@ -0,0 +1,13 @@
+package com.zendev.moodflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodFlixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoodFlixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zendev/moodflix/MoodFlixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zendev.moodflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoodFlixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
