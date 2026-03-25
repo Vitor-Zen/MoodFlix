64f4c1b9cb06334acced28b0e55bfb99a4f730e8
Vitor Zen <vitorzen19@gmail.com>
2026-03-25 00:33:48 -0300

    feat: add jikan service

---
+++ src/main/java/com/zendev/moodflix/service/AnimesService.java
@@ -0,0 +1,58 @@
+package com.zendev.moodflix.service;
+
+import com.zendev.moodflix.config.jikan.AnimesMoodConfig;
+import com.zendev.moodflix.dto.jikan.AnimePageResponse;
+import com.zendev.moodflix.dto.jikan.AnimeResponse;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+import java.util.List;
+
+@Service
+public class AnimesService {
+
+    private final RestClient restClient;
+    private final AnimesMoodConfig animesMoodConfig;
+
+    public AnimesService(@Qualifier("jikanRestClient") RestClient restClient, AnimesMoodConfig animesMoodConfig) {
+        this.restClient = restClient;
+        this.animesMoodConfig = animesMoodConfig;
+    }
+
+    public List<AnimeResponse> getAnimesByMood(String mood, int page){
+        String genres = animesMoodConfig.getGenres().get(mood);
+
+        AnimePageResponse response = restClient
+                .get()
+                .uri("/anime?genres={genres}&sfw=true&min_score=7&order_by=score&sort=desc&page={page}", genres, page)
+                .retrieve()
+                .body(new ParameterizedTypeReference<AnimePageResponse>() {});
+
+        return response != null ? response.data() : List.of();
+    }
+
+    public List<AnimeResponse> searchAnimesByName(String name) {
+        AnimePageResponse response = restClient
+                .get()
+                .uri("/anime?q={name}&sfw=true&type=tv", name)
+                .retrieve()
+                .body(new ParameterizedTypeReference<AnimePageResponse>() {});
+
+        return response!= null
+                ? response.data().stream().filter(anime -> "TV".equals(anime.type()) || "Movie".equals(anime.type()))
+                .toList()
+                : List.of();
+    }
+
+    public List<AnimeResponse> getAnimeRecommendations(int malId) {
+        AnimePageResponse response = restClient
+                .get()
+                .uri("/anime/{malId}/recommendations", malId)
+                .retrieve()
+                .body(new ParameterizedTypeReference<AnimePageResponse>() {});
+
+        return response != null ? response.data() : List.of();
+    }
+}
