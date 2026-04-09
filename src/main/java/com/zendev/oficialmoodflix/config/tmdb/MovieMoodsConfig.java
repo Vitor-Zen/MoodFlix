c3c9844b20730d905495eb0b38720c7e9eb3bc90
Vitor-Zen <vitorzen19@gmail.com>
2026-04-09 18:43:47 -0300

    feat: add MovieMoodsConfig

---
+++ src/main/java/com/zendev/oficialmoodflix/config/tmdb/MovieMoodsConfig.java
@@ -0,0 +1,20 @@
+package com.zendev.oficialmoodflix.config.tmdb;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+
+@Configuration
+@ConfigurationProperties(prefix = "moviesmood")
+public class MovieMoodsConfig {
+    private Map<String, String> genres;
+
+    public Map<String, String> getGenres(){
+        return genres;
+    }
+
+    public void setGenres(Map<String, String> genres) {
+        this.genres = genres;
+    }
+}
